195bf9ddef22dd3227a06c3b821ab01cf9f58bd0
Dave <davediemert_88@hotmail.com>
2022-05-03 21:07:22 -0700

    computer is working on a pretty hard basis. one way to beat it i have found. easy random playing mode is also available but need to program buttons for difficulty mode.

--- script.js
+++ script.js
@@ -27,15 +27,18 @@ let game = (()=>{
         if (win.winner == true){
             return;
         }
+        //already played in square
         if (squareSelected.value != 0) {
             dom.inputs.topH2.innerHTML = "You must play in an un-occupied square";
             return;
         }
+        //player 1's turn
         if ((game.turnCounter == 0) || (game.turnCounter % 2 == 0)){
         squareSelected.InnerContent(`${player.player1.marker}`);
         dom.inputs.topH2.innerHTML = `It is ${player.player2.name}'s turn now.`
         squareSelected.value = 1;
         }
+        //player 2's turn
         else {
             squareSelected.InnerContent(`${player.player2.marker}`)
             dom.inputs.topH2.innerHTML = `It is ${player.player1.name}'s turn now.`
@@ -45,20 +48,20 @@ let game = (()=>{
         game.turnCounter++;
         win.checkForWin();
         PlayAgain();
-        
+        if ((player.player2.HumanComp == "comp2") && (win.winner == false)){
+            //computer.randomPlay(); super easy mode
+            computer.findBestMove();
+        }
 
     }
 
 
     //building game board function
-    function makeSection(section){
+    function makeSection(section,r1,r2,r3,c1,c2,c3,d1,d2){
         let createNewSquare = document.createElement('div');
         createNewSquare.classList.add("board", section);
         dom.inputs.container.appendChild(createNewSquare);
         createNewSquare = document.querySelector(`.${section}`)
-
-        
-
     // The return object
         let tic_tac_toe_square = {
         placement: section,
@@ -68,7 +71,15 @@ let game = (()=>{
             let innerContent2 = document.querySelector(`.${this.placement}`);
             innerContent2.innerHTML = newInnerHTML;
             this.currentInnerContent = innerContent2.innerHTML;
-        }
+        },
+        row1: r1,
+        row2: r2,
+        row3: r3,
+        column1: c1,
+        column2: c2,
+        column3: c3,
+        diag1: d1,
+        diag2: d2, 
         }
         //adding event listener to the newly created div
         createNewSquare.addEventListener('click', function(){selectSquare(tic_tac_toe_square)})
@@ -79,30 +90,30 @@ let game = (()=>{
     
    // the game board setup
     let gameBoard = [
-    new makeSection("topLeft"),
-    new makeSection("topMid"),
-    new makeSection("topRight"),
-    new makeSection("midLeft"),
-    new makeSection("midMid"),
-    new makeSection("midRight"),
-    new makeSection("bottomLeft"),
-    new makeSection("bottomMid"),
-    new makeSection("bottomRight"),
-]
+        new makeSection("topLeft",1,0,0,1,0,0,1,0),
+        new makeSection("topMid",1,0,0,0,1,0,0,0),
+        new makeSection("topRight",1,0,0,0,0,1,0,1),
+        new makeSection("midLeft",0,1,0,1,0,0,0,0),
+        new makeSection("midMid",0,1,0,0,1,0,1,1),
+        new makeSection("midRight",0,1,0,0,0,1,0,0),
+        new makeSection("bottomLeft",0,0,1,1,0,0,0,1),
+        new makeSection("bottomMid",0,0,1,0,1,0,0,0),
+        new makeSection("bottomRight",0,0,1,0,0,1,1,0),
+    ]
 
 //function to reset the gameBoard
     function resetGameBoard(){
         game.gameBoard = [];
         game.gameBoard = [
-            new makeSection("topLeft"),
-            new makeSection("topMid"),
-            new makeSection("topRight"),
-            new makeSection("midLeft"),
-            new makeSection("midMid"),
-            new makeSection("midRight"),
-            new makeSection("bottomLeft"),
-            new makeSection("bottomMid"),
-            new makeSection("bottomRight"),
+            new makeSection("topLeft",1,0,0,1,0,0,1,0),
+            new makeSection("topMid",1,0,0,0,1,0,0,0),
+            new makeSection("topRight",1,0,0,0,0,1,0,1),
+            new makeSection("midLeft",0,1,0,1,0,0,0,0),
+            new makeSection("midMid",0,1,0,0,1,0,1,1),
+            new makeSection("midRight",0,1,0,0,0,1,0,0),
+            new makeSection("bottomLeft",0,0,1,1,0,0,0,1),
+            new makeSection("bottomMid",0,0,1,0,1,0,0,0),
+            new makeSection("bottomRight",0,0,1,0,0,1,1,0),
         ]
     }
 //function to reset win condition object turn counter and winner boolean
@@ -145,7 +156,7 @@ function resetWinConditions(){
         }
     }
 
-    return {gameBoard, turnCounter, resetGame};
+    return {gameBoard, turnCounter, resetGame, PlayAgain};
     })();
 
 
@@ -227,10 +238,12 @@ function updateWins(squareSelected) {
         let player2Name = document.getElementById("name2");
         let player1CatchPhrase = document.getElementById("phrase1");
         let player2CatchPhrase = document.getElementById("phrase2");
+        let player1HumanComp = document.getElementById("humanComp1");
+        let player2HumanComp = document.getElementById("humanComp2");
         
         function sameMarkers(){
             if (player1.marker == player2.marker){
-                alert("choose different markers")
+                dom.inputs.topH2.innerHTML = `Please choose different markers.`
                 player1.chose = false;
                 player2.chose = false;
                 player1.marker = "a";
@@ -245,29 +258,27 @@ function updateWins(squareSelected) {
             if ((player1.chose == true) && (player2.chose == true)){
                 dom.inputs.playerSelection.classList.toggle('hidden');
                 dom.inputs.container.classList.toggle('hidden');
-                dom.inputs.popUp.classList.toggle('hidden');
                 dom.inputs.topH2.innerHTML = `${player.player1.name} goes first.`
-
             }
             else return;
         }
 
         let lockPlayer1Handler = function(){
+            player1.HumanComp = player1HumanComp.value
             player1.name = player1Name.value;
             player1.marker = player1Marker.value;
             player1.catchPhrase = player1CatchPhrase.value;
             player1.chose = true;
-            console.log(player1);
             player1LockButton.removeEventListener('click', lockPlayer1Handler);
             bothPlayersLocked()
 
         }
         let lockPlayer2Handler = function(){
+            player2.HumanComp = player2HumanComp.value
             player2.name = player2Name.value;
             player2.marker = player2Marker.value;
             player2.catchPhrase = player2CatchPhrase.value;
             player2.chose = true;
-            console.log(player2);
             player2LockButton.removeEventListener('click', lockPlayer2Handler);
             bothPlayersLocked()
         }
@@ -280,4 +291,100 @@ function updateWins(squareSelected) {
 
     })();
 
+/******************************************************************************
+ * ******* Module for the computer player*************************************
+ *****************************************************************************/
+const computer = (() => {
+    let computerPlayer1Chosen = document.getElementById("humanComp1");
+    let computerPlayer2Chosen = document.getElementById("humanComp2");
+    
+    function compsTurn(){
+        checkEmptySquare();
+        findBestMove();
+    }
+    //Find available moves
+    function checkEmptySquare(){
+        let allSquares = Array.from(game.gameBoard);
+        let availableMoves = allSquares.filter(square => square.value == 0)
+        return availableMoves;
+    }
 
+    //find best available move
+    function findBestMove() {
+        let bestMove;
+        if ((game.turnCounter == 1) && (game.gameBoard[4].value == 0)){
+            bestMove = game.gameBoard[4];
+        }
+        else  {
+            bestMove = winOrBlock();
+        }
+        updateCompMove(bestMove);
+        return bestMove;
+    }
+    //Can the computer win or block?
+    function winOrBlock(){
+        let winMove;
+        let blockMove;
+        let winBlockFinalMove;
+        let winKeys = Object.keys(win.winConditions)
+        winKeys.forEach((key) => {
+            //if comp can win
+            if (win.winConditions[key] === 20){
+                console.log(`I can win in ${key} next turn`)
+                let possibilities = checkEmptySquare();
+                console.log(possibilities)
+                possibilities.forEach(square => {
+                    if (square[key] == 1){
+                        winMove = square;
+                    }
+                })
+            }
+            //cant win, it must block a win
+            else if (win.winConditions[key] === 2){
+                console.log(`I have to block in ${key} or I will lose`);
+                let possibilities = checkEmptySquare();
+                console.log(possibilities)
+                possibilities.forEach(square => {
+                    if (square[key] == 1){
+                        blockMove = square;
+                    }
+                })
+            }
+        if (winMove){
+            winBlockFinalMove = winMove
+        }
+        else if (blockMove){
+            winBlockFinalMove = blockMove
+        }
+        //if no win or block move, randomly move
+        else if (!winMove && !blockMove){
+            let availableMoves = checkEmptySquare();
+            let myMove = availableMoves.splice((Math.floor((Math.random() *availableMoves.length))), 1);
+            winBlockFinalMove = myMove[0];
+        }
+
+        })
+    return winBlockFinalMove
+    }
+
+
+    
+    //play randomly (EASY MODE)
+    function randomPlay(){
+    let availableMoves = checkEmptySquare();
+    let myMove = availableMoves.splice((Math.floor((Math.random() *availableMoves.length))), 1);
+    updateCompMove(myMove[0]);
+}
+
+function updateCompMove(compMove){
+    compMove.InnerContent(`${player.player2.marker}`)
+    dom.inputs.topH2.innerHTML = `It is ${player.player1.name}'s turn now.`
+    compMove.value = 10;
+    win.updateWins(compMove);
+    game.turnCounter++;
+    win.checkForWin();
+    game.PlayAgain();
+}
+
+    return {checkEmptySquare, findBestMove, winOrBlock, randomPlay, }
+})();
